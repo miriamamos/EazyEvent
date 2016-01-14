2b8e94af0793745f172d5b08725526d32518898d
miriamamos <miriamba99@gmail.com>
2016-01-14 22:00:16 +0200

    all project

---
+++ EasyEvent/JS/jsHome.js
@@ -0,0 +1,15 @@
+﻿
+       $(function () {
+            $('a').click(function () {
+                var operation = $(this).attr('id');
+                var StringName = operation.toString() + ".html".toString();
+                $("#viewPage").load(StringName);
+                $('footer').attr('style', 'display: block !important');
+
+            });
+            $("#btnReturn").click(function () {
+                window.location.href = "Home.html"
+            });
+        });
+ 
+
