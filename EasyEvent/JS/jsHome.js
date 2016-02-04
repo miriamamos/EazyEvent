c686444e13d3b8b940ed19579d8ccccf97c948fd
miriamamos <miriamba99@gmail.com>
2016-02-05 01:14:22 +0200

    בדיקות תקינות של שם משתמש וסיסמה

--- EasyEvent/JS/jsHome.js
+++ EasyEvent/JS/jsHome.js
@@ -1,63 +1,52 @@
-﻿/// <reference path="../html/MakeEvent.html" />
-/// <reference path="../html/CheckParticipation.html" />
-/// <reference path="../html/ManagementEvent.html" />
-/// <reference path="../html/MakeEvent.html" />
-/// <reference path="../html/CheckParticipation.html" />
-/// <reference path="../html/MakeEvent.html" />
-
-
-
+﻿
 $(function () {
+   
+
     //מעבר לדפים
     $('a').click(function () {
         var operation = $(this).attr('id');
         checkPassword()
         debugger;
-        //var StringName = window.location + 'html/' + operation + '.html'.toString();
+        var StringName = 'html/' + operation + '.html'.toString();
         goToForm(operation)
-        //this.href.replace(window.location, operation + '.html'.toString());
-        $("#viewPage").append(window.location, operation + '.html'.toString());
-        //$("#viewPage").load(StringName);
+        $("#viewPage").load(StringName);
         $('footer').attr('style', 'display: block !important');
 
     });
+ 
 
     //חזרה לדף ראשי
     $("#btnReturn").click(function () {
         window.location.href = "Home.html"
     });
 
-    //בדיקת שם משתמש וסיסמה
-    function checkPassword() {          
-    }
-
-    //בדיקת הכנסת  שם משתמש וסיסמה
-    $('.checkPassword').click(function () {
-        checkSign();
-    })
-
-    //בדיקת הכנסת  שם משתמש וסיסמה
-    function checkSign() {
-        //בדיקה על אימות שם משתמש וסיסמה
-        //בדיקה האם קיימת הסיסמה למשתמש חדש
-        //בדיקה כאשר מכניס משתמש קיים
-    }
 
     //מעבר לדפים   
     function goToForm(operation) {
-        //switch (switch_on) {
-        //    default:
-
-        //}
         //לאיזה דף מועבר
 
         //יצירת אירוע ישר מעבר
-         
+
         //ניהול אירוע עם הבאת נתונים
 
-        //הצגת אירוע??????
-       //בדיקות תקינות
+        //הצגת אירוע
+        //בדיקות תקינות
+        switch (operation) {
+            case "MakeEvent": {
+                //ניהול אירוע עם הבאת נתונים
+            }
+            case "ManagetEvent": {
+            }
+            case "viewEvent": {
+            }
+            default:
+
+        }
+
     }
+
+
+  
 });
 
 
